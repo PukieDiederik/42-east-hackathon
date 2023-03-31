74c38ef6fd7cee6a247e9930cdc2651a705edb89
Diederik Robert marie boeren <drobert-@c2r10s6.42lisboa.com>
2023-03-31 15:33:09 +0100

    Added small express server

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('hello world!');
+});
+
+app.listen(process.env.PORT, () => 
+{
+	console.log(`Started server and listening on port ${process.env.PORT}`);
+});
+
