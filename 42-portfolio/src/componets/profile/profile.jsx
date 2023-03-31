9cdc54f6c919eee2d518c1781f48862d92788a29
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-03-31 15:36:45 +0100

    initial commit

---
+++ 42-portfolio/src/componets/profile/profile.jsx
@@ -0,0 +1,10 @@
+import "./profile.css"
+export function Profile() {
+  return (
+    <header className="Profile">
+    <img src="https://cdn.pixabay.com/photo/2023/03/02/14/46/pit-bull-7825554_1280.jpg" alt="Profile" />
+      <h1>Name</h1>
+      <h2>42 Student</h2>
+    </header>
+  )
+}
---
+++ 42-portfolio/src/componets/projects/project.jsx
@@ -0,0 +1,11 @@
+export function Project() {
+  return (
+    <section className="project">
+    <h1>Project</h1>
+    <article>
+      <h2>Project 1</h2>
+      <p>Project 1 description</p>
+    </article>
+    </section>
+  )
+}
