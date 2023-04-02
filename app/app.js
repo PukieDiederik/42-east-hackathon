a01558ac152c62cff5b9db0cf17a4cbf82cfe8da
JustShush <marquesdinis.p@gmail.com>
2023-04-02 13:38:30 +0100

    Implemented a check if the project is validated. And project output is now sorted

--- app/app.js
+++ app/app.js
@@ -16,7 +16,6 @@ app.get('/user/:user', (req, res) => {
 
 	console.log("Getting data for: " + req.params.user);
 
-
 	// Get basic info about user
 	const xhr = new XMLHttpRequest();
 	xhr.withCredentials = true;
@@ -25,6 +24,7 @@ app.get('/user/:user', (req, res) => {
 	xhr.send();
 
 	res_json = JSON.parse(xhr.responseText);
+	// console.log(res_json);
 
 	// update data with new info
 	data.user_id = res_json.id;
@@ -37,8 +37,7 @@ app.get('/user/:user', (req, res) => {
 
 	// Find project ids
 	let project_ids = []
-	for (project of res_json.projects_users)
-	{
+	for (project of res_json.projects_users) {
 		//console.log(project)
 		// Tried to get validated instead of status
 		/* if (project.validated === true)
@@ -58,59 +57,91 @@ app.get('/user/:user', (req, res) => {
 	// Add projects to data
 	data.projects = []
 	project_json = JSON.parse(xhr_p.responseText);
-	for (project of project_json)
-	{
+	for (project of project_json) {
 		let is_piscine = false;
 		// Check if we should add this project to the list
 		if (project.exam == true)
-		continue;
-		for(cursus of project.cursus)
-		{
-			if (cursus.kind == 'piscine')
-			{
+			continue;
+		
+		// Check if the project is validated
+		let r = res_json.projects_users.filter(function (item) {
+			return item.project.id == project.id;
+		})[0];
+		if (r['validated?'] == false)
+			continue;
+
+
+		for (cursus of project.cursus) {
+			if (cursus.kind == 'piscine') {
 				is_piscine = true;
 				break;
 			}
 		}
-		
+
 		// Add all the keywords from that project to the json (could have)
 		let keywordsArr = [];
 		project.project_sessions[0].objectives.forEach(key => {
 			keywordsArr.push(key);
 		});
-		
+
 		// Add project to list if it isn't a piscine
-		if (!is_piscine)
-		{
+		if (!is_piscine) {
+			// if the project is in groups or not
+			let group = false;
+			if (project.project_sessions[0].solo === false) group = true;
+			// difficulty of the project
 			let difficulty = 0;
 			difficulty = project.project_sessions[0].difficulty;
 			/* if(difficulty <= 0) {
 				difficulty = 100;
 			} */
+			// if the user did the bonus
+			let bonus = false;
+			res_json.projects_users.forEach(pro => {
+				if(pro.project.id === project.id) {
+					if (pro.final_mark > 100) bonus = true;
+					//console.log(`name: ${pro.project.name} -- bonus: ${bonus}`)
+				}
+			});
 			data.projects.push({
 				"name": project.name,
 				"description": project.project_sessions[0].description,
 				"keywords": keywordsArr,
 				"difficulty": difficulty,
-		});}
+				"group": group,
+				"bonus": bonus,
+			});
+		}
 	}
-	
+
+	// Sort projects based on difficulty first, then name
+	data.projects.sort(function (lhs, rhs)
+	{
+		if (lhs.difficulty < rhs.difficulty)
+			return -1;
+		if (lhs.difficulty > rhs.difficulty)
+			return 1;
+		if (lhs.name < rhs.name)
+			return -1;
+		if (lhs.name > rhs.name)
+			return 1;
+		return 0;
+	});
+
 	//return data
 	res.json(data);
 });
 
-app.listen(process.env.PORT, () => 
-{
+app.listen(process.env.PORT, () => {
 	console.log(`Started server and listening on port ${process.env.PORT}`);
-	
+
 	// Get the access token
 	const req = new XMLHttpRequest();
 	req.withCredentials = true;
 	req.open("POST", "https://api.intra.42.fr/oauth/token", false);
 	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	req.send(`grant_type=client_credentials&client_id=${process.env.API42_UID}&client_secret=${process.env.API42_SECRET}`);
-	
+
 	access_token = JSON.parse(req.responseText).access_token;
 	console.log("Obtained access token\n" + access_token);
 });
-
